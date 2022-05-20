addb17893a50ebec715abc074f8e14518d2b51d0
zjj-comeon <1753524606@qq.com>
2022-05-20 09:34:42 +0800

    init backend

---
+++ ElectiveSystem/src/main/java/com/ap/electivesystem/ElectiveSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ap.electivesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectiveSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectiveSystemApplication.class, args);
+    }
+
+}
---
+++ ElectiveSystem/src/test/java/com/ap/electivesystem/ElectiveSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.electivesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectiveSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
