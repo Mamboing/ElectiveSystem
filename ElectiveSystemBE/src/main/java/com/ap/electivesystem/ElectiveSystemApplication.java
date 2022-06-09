90ae8265f7487aa13489f9a2d8f2bd4fdfa66699
mamboing <1302855173@qq.com>
2022-06-10 00:48:48 +0800

    Add Log4j2 module

--- ElectiveSystemBE/src/main/java/com/ap/electivesystem/ElectiveSystemApplication.java
+++ ElectiveSystemBE/src/main/java/com/ap/electivesystem/ElectiveSystemApplication.java
@@ -1,13 +1,16 @@
 package com.ap.electivesystem;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ElectiveSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElectiveSystemApplication.class, args);
+        log.info("ElectiveSystemServer启动成功");
     }
 
 }
