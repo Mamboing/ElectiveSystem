6fd775a46ae9cc59514aeb4202355fae7d58b472
mamboing <1302855173@qq.com>
2022-06-12 22:30:57 +0800

    merge progress

--- ElectiveSystemBE/src/main/java/com/ap/electivesystem/ElectiveSystemApplication.java
+++ ElectiveSystemBE/src/main/java/com/ap/electivesystem/ElectiveSystemApplication.java
@@ -1,15 +1,19 @@
 package com.ap.electivesystem;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 @MapperScan("com.ap.electivesystem.mapper")
 public class ElectiveSystemApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ElectiveSystemApplication.class, args);
+        log.info("ElectiveSystemServer启动成功");
     }
 
 }
---
+++ ElectiveSystemBE/src/main/java/com/ap/electivesystem/controller/TestRestController.java
@@ -0,0 +1,27 @@
+package com.ap.electivesystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestRestController {
+    @RequestMapping("/getSessionId")
+    public Map<String, Object> getSessionId(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, Object> map = new HashMap<>(4);
+
+        HttpSession session = request.getSession();
+        String id = session.getId();
+
+        int port = request.getServerPort();
+        map.put("sessionId", id);
+        map.put("serverPort", port);
+
+        return map;
+    }
+}
