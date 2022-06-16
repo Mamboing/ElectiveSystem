fa8496ec92847399d9b9609c1949c5e0660a2e5a
zjj-comeon <1753524606@qq.com>
2022-06-16 17:26:19 +0800

    更新管理员信息

--- ElectiveSystemBE/src/main/java/com/ap/electivesystem/controller/admin/AdminController.java
+++ ElectiveSystemBE/src/main/java/com/ap/electivesystem/controller/admin/AdminController.java
@@ -4,9 +4,8 @@ import com.ap.electivesystem.config.auth.annotation.Register;
 import com.ap.electivesystem.entity.Admin;
 import com.ap.electivesystem.entity.vo.ResultVO;
 import com.ap.electivesystem.service.AdminService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ap.electivesystem.utils.Md5Encrypt;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.annotation.Resource;
@@ -18,13 +17,24 @@ import java.util.List;
 @ApiIgnore
 public class AdminController {
 
+    public static final String PASSWORD_SALT = "Elective_System_0.0";
+
     @Resource
     private AdminService adminService;
+    @Resource
+    private Md5Encrypt md5Encrypt;
+
 
     @GetMapping("/list")
     public ResultVO list() {
         List<Admin> list = adminService.list();
         return ResultVO.success(list);
+    }
+
+    @PostMapping("/update")
+    public ResultVO update(@RequestBody Admin admin){
+        admin.setAdminPass(md5Encrypt.encode(admin.getAdminPass(), PASSWORD_SALT));
+        return ResultVO.success(adminService.updateById(admin));
     }
 
 }
